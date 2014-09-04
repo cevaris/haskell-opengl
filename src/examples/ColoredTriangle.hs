ef5a3ea6fe785db0c510f79f6cf9c496b761b955
Adam Cardenas <cevaris@gmail.com>
2014-09-04 16:25:04 -0600

    Found out how to link modules from another class to a single executable via cabal

--- src/examples/ColoredTriangle.hs
+++ src/examples/ColoredTriangle.hs
@@ -2,21 +2,9 @@ import Graphics.UI.GLUT
 
 data CustomColor = CustomColor {r::GLfloat, g::GLfloat, b::GLfloat} deriving (Read, Show, Eq)
 
---r :: CustomColor -> GLfloat
-
---type RColor = GLfloat
---type GColor = GLfloat
---type BColor = GLfloat
-
---data CustomColor = CustomColor RColor GColor BColor deriving (Read, Show, Eq)
-
 red   = CustomColor 1 0 0
 green = CustomColor 0 1 0
 blue  = CustomColor 0 0 1
-
---red   = 1 0 0 :: CustomColor
---green = 0 1 0 :: CustomColor
---blue  = 0 0 1 :: CustomColor
 
 trianglePoints :: [(GLfloat,GLfloat,GLfloat,CustomColor)]
 trianglePoints = [(0, 0.5, 0, blue), (0.5, (-0.5), 0, green), ((-0.5), (-0.5), 0, red)]
@@ -26,12 +14,19 @@ main = do
   (_progName, _args) <- getArgsAndInitialize
   _window <- createWindow "Hello World"
   displayCallback $= display
+  reshapeCallback $= Just reshape
   mainLoop
 
-
+reshape :: ReshapeCallback
+reshape size = do
+  viewport $= (Position 0 0, size)
+  postRedisplay Nothing
+  
+-- Set color
 color3f :: CustomColor -> IO ()
 color3f c = color $ Color3 (r c) (g c) (b c)
 
+-- Set Vertex
 vertex3f :: GLfloat -> GLfloat -> GLfloat -> IO ()
 vertex3f x y z = vertex $ Vertex3 x y z
 
@@ -41,10 +36,10 @@ display = do
 
   -- http://stackoverflow.com/questions/9300773/drawing-lines-with-opengl-in-haskell#
   renderPrimitive Polygon $ do
-    mapM_ (\(x, y, z, c) ->  do 
+    mapM_ (
+      \(x, y, z, c) -> do 
         color3f  c
-        vertex3f x y z
-      ) trianglePoints
+        vertex3f x y z ) trianglePoints
   flush
 
 
