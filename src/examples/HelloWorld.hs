0f696eb4ebd443240c465a9e1c978781e2d7582a
Adam Cardenas <cevaris@gmail.com>
2014-09-03 22:52:24 -0600

    added drawing example

--- src/examples/HelloWorld.hs
+++ src/examples/HelloWorld.hs
@@ -4,6 +4,8 @@ main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
   window <- createWindow "Hello World Sammy!!"
+
+  -- displayCallback is a globally mutable StateVar type
   displayCallback $= display
   -- GLUT takes over
   mainLoop 
