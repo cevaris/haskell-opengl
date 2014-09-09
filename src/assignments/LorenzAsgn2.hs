e081e0c57f47b069cfaf384296bb74e6afa1b89a
Adam Cardenas <cevaris@gmail.com>
2014-09-08 22:10:51 -0600

    refactoring lorenz

--- src/assignments/LorenzAsgn2.hs
+++ src/assignments/LorenzAsgn2.hs
@@ -1,16 +1,23 @@
 import Graphics.UI.GLUT
 
-data Lorenz = Lorenz {step::Integer, x::Float, y::Float, z::Float} deriving (Read, Show, Eq)
 
 s  = 10
 b  = 2.6666
 r  = 28
 dt = 0.001
 
+
+data Lorenz = Lorenz {step::Integer, x::Float, y::Float, z::Float} deriving (Read, Show, Eq)
+--data LorenzParams = LorenzParams {s:: Integer, b:: Float, r:: Integer, dt:: Float, limit:: Integer} deriving (Read, Show, Eq)
+
+lzBase   = Lorenz 0 1 1 1
+--lzParams = 10 2.6666 28 0.001 5
+
+
 lorenz  :: Float -> [Lorenz]
-lorenz  dt = go (Lorenz 0 1 1 1) [(Lorenz 0 1 1 1)]
+lorenz  dt = go lzBase [lzBase]
         where 
           go :: Lorenz -> [Lorenz] -> [Lorenz]
           go (Lorenz 10 _ _ _) xs = reverse xs
-          go (Lorenz i x y z)  xs = let l = (Lorenz (i+1) (x+dt*(s*(y-x))) (y+dt*(x*(r-z)-y)) (z+dt*(x*y-b*z)))
+          go (Lorenz i x y z)  xs = let l = Lorenz (i+1) (x+dt*(s*(y-x))) (y+dt*(x*(r-z)-y)) (z+dt*(x*y-b*z))
                                     in go l (l:xs)
