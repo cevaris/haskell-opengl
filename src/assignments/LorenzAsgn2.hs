2ff2726be8defe22e74c15d0f158ecc435638ed0
Adam Cardenas <cevaris@gmail.com>
2014-09-09 09:37:24 -0600

    added state

--- src/assignments/LorenzAsgn2.hs
+++ src/assignments/LorenzAsgn2.hs
@@ -1,4 +1,30 @@
 import Graphics.UI.GLUT
+import Data.IORef ( IORef, newIORef )
+import System.Exit ( exitWith, ExitCode(ExitSuccess), exitFailure )
+
+----------------------------------------------------------------------------------------------------------------
+-- Global State
+type View = (GLfloat, GLfloat, GLfloat)
+
+data State = State {
+   frames  :: IORef Int,
+   t0      :: IORef Int,
+   viewRot :: IORef View,
+   angle'  :: IORef GLfloat }
+
+makeState :: IO State
+makeState = do
+   f <- newIORef 0
+   t <- newIORef 0
+   v <- newIORef (20, 30, 0)
+   a <- newIORef 0
+   return $ State { frames = f, t0 = t, viewRot = v, angle' = a }
+
+----------------------------------------------------------------------------------------------------------------
+
+
+----------------------------------------------------------------------------------------------------------------
+-- Lorenz
 
 s  = 10
 b  = 2.6666
@@ -19,6 +45,29 @@ lorenz  dt = go lzBase [lzBase]
 
 lorenzPoints :: [(GLfloat,GLfloat,GLfloat)]
 lorenzPoints = map (\(Lorenz i x y z) -> ((realToFrac (x/10) :: GLfloat), (realToFrac (y/10) :: GLfloat), (realToFrac (z/10) :: GLfloat))) (lorenz 0.001)
+
+----------------------------------------------------------------------------------------------------------------
+
+
+----------------------------------------------------------------------------------------------------------------
+-- Key Binding
+
+keyboard :: State -> KeyboardMouseCallback
+keyboard state (Char 'z')           _ _ _ = modRot state ( 0,  0,  5)
+keyboard state (Char 'Z')           _ _ _ = modRot state ( 0,  0, -5)
+keyboard state (SpecialKey KeyUp)   _ _ _ = modRot state ( 5,  0,  0)
+keyboard state (SpecialKey KeyDown) _ _ _ = modRot state (-5,  0,  0)
+keyboard state (SpecialKey KeyLeft) _ _ _ = modRot state ( 0,  5,  0)
+keyboard state (SpecialKey KeyRight)_ _ _ = modRot state ( 0, -5,  0)
+keyboard _     (Char '\27')         _ _ _ = exitWith ExitSuccess
+keyboard _     _                    _ _ _ = return ()
+
+modRot :: State -> View -> IO ()
+modRot state (dx,dy,dz) = do
+   (x, y, z) <- get (viewRot state)
+   viewRot state $= (x + dx, y + dy, z + dz)
+   postRedisplay Nothing
+
 
 main :: IO ()
 main = do
@@ -44,6 +93,7 @@ vertex3f x y z = vertex $ Vertex3 x y z
 display :: DisplayCallback
 display = do 
   clear [ColorBuffer]
+  state <- makeState
 
   -- http://stackoverflow.com/questions/9300773/drawing-lines-with-opengl-in-haskell#
   renderPrimitive LineStrip $ do
