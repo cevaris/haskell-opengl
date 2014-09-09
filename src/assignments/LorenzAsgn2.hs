6b51330e046de30fed99352f7cd22e7ffc3b80ce
Adam Cardenas <cevaris@gmail.com>
2014-09-08 21:57:48 -0600

    refactoring lorenz

--- src/assignments/LorenzAsgn2.hs
+++ src/assignments/LorenzAsgn2.hs
@@ -8,14 +8,15 @@ r  = 28
 dt = 0.001
 
 lorenz  :: Float -> [Lorenz]
-lorenz  dt = go 0 1 1 1 [Lorenz 0 1 1 1]
+lorenz  dt = go (Lorenz 0 1 1 1) [(Lorenz 0 1 1 1)]
         where 
-          go :: Integer -> Float -> Float -> Float -> [Lorenz] -> [Lorenz]
-          go 10   _ _ _ xs = reverse xs
-          go step x y z xs = let dx = s*(y-x)
-                                 dy = x*(r-z)-y
-                                 dz = x*y-b*z
-                                 x' = x+dt*dx
-                                 y' = y+dt*dy
-                                 z' = z+dt*dz
-                             in go (step+1) x' y' z' (Lorenz (step+1) x' y' z':xs)
+          go :: Lorenz -> [Lorenz] -> [Lorenz]
+          go (Lorenz 10 _ _ _) xs = reverse xs
+          go (Lorenz i x y z)  xs = let dx = s*(y-x)
+                                        dy = x*(r-z)-y
+                                        dz = x*y-b*z
+                                        x' = x+dt*dx
+                                        y' = y+dt*dy
+                                        z' = z+dt*dz
+                                        l  = (Lorenz (i+1) x' y' z')
+                                    in go l (l:xs)
