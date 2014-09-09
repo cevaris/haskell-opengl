cd138bc9e3ded51e73374bfa80e970779f05fa7d
Adam Cardenas <cevaris@gmail.com>
2014-09-08 21:39:39 -0600

    got working lorenz

--- src/assignments/LorenzAsgn2.hs
+++ src/assignments/LorenzAsgn2.hs
@@ -43,18 +43,22 @@
 
 --data Lorenz = Lorenz {step::Integer, x::GLfloat, y::GLfloat, z::GLfloat} deriving (Read, Show, Eq)
 
-s = 10
-b = 2.6666
-r = 28
+s  = 10
+b  = 2.6666
+r  = 28
+dt = 0.001
 lorenz  :: Float -> [(Integer, Float, Float, Float)]
-lorenz  dt = go 1 1 1 10 []
+lorenz  dt = go 1 1 1 0 [(0, 1, 1, 1)]
         where 
           go :: Float -> Float -> Float -> Integer -> [(Integer, Float, Float, Float)] -> [(Integer, Float, Float, Float)]
-          go _ _ _ 0    xs = (0, 1, 1, 1):xs
-          go x y z step xs = let x' = (x+(dt*(s*(y-x))))
-                                 y' = (y+(dt*(x*(r-z))-y))
-                                 z' = (z+(dt*(x*y-b*z)))
-                             in go x' y' z' (step-1) ((step, x', y', z'):xs)
+          go _ _ _ 10    xs = reverse xs
+          go x y z step xs = let dx = s*(y-x)
+                                 dy = x*(r-z)-y
+                                 dz = x*y - b*z
+                                 x' = x+dt*dx
+                                 y' = y+dt*dy
+                                 z' = z+dt*dz
+                             in go x' y' z' (step+1) ((step+1, x', y', z'):xs)
 
           --go x y z step xs = do 
           --  let s  = 10
